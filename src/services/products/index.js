a1c5d21251ef0d1f8c17aeeb73a81e87e1802233
Yvette <nchombuayvta@gmil.com>
2020-06-29 16:44:16 +0200

    got reviews for a single product

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -14,6 +14,16 @@ const getProducts = () =>{
     
 }
 
+const reviewsFilePath = path.join(__dirname, "../reviews/reviews.json")
+const getReviews = () =>{
+    const reviewsFileContent= fs.readFileSync(reviewsFilePath)
+    const reviewsContent = reviewsFileContent.toString()
+    const reviews = JSON.parse(reviewsContent)
+    console.log("review is",reviews)
+    return reviews
+    
+}
+
 //1. Get products
 productsRouter.get("/", (req,res,next) =>{
     try{
@@ -27,12 +37,43 @@ productsRouter.get("/", (req,res,next) =>{
     }
     } catch(error){
         console.log(error)
-        const err = new Error("While getting products list a problem occured")
-        next()
+        const err = new Error
+        error.httpStatusCode = 404
+        next(err)
     }      
 })
 
 //get a specific pdt
+productsRouter.get("/:id", (req,res,next)=>{
+    try{
+        const products = getProducts()
+        const product = products.filter(pdt => pdt.id === req.params.id)
+        if(product){
+            res.send(product)
+        }else{
+            const error = new Error()
+            error.httpStatusCode = 404
+            next(error)
+        }
+
+    } catch(error){
+       console.log(error)
+       next("while getting products list a problem occured!")
+    }
+    
+})
+
+// get reviews for a specific product
+productsRouter.get("/:id/reviews", (req,res,next)=>{
+    try{
+    const reviews = getReviews()
+    const filteredReview = reviews.filter(review =>review.elementId === req.params.id)
+
+    }catch(error){
+        console.log(error)
+        next("while getting reviews a problem occured!")
+    }  
+})
 
 
 module.exports = productsRouter
